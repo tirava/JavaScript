53c98a1d13804b813de2c174b5b0302062014ca5
Eugene Klimov <tirava@gmail.com>
2019-10-18 20:21:17 +0300

    HW-4 ready

--- 4/foo.js
+++ 4/foo.js
@@ -3,22 +3,20 @@
 // foo(5)() = 5
 // foo(2)(1)(3)(4)() = 24
 
-let result;
-
-const foo = function (x) {
-    if (!result || result === 0) {
-        result = 1;
+function foo(x) {
+    if (!foo.result) {
+        foo.result = 1;
     }
     if (!x) {
-        const tmp = result;
-        result = 1;
-        return tmp
+        const tmp = foo.result;
+        foo.result = 1;
+        return tmp;
     }
-    result *= x;
+    foo.result *= x;
     return function (y) {
-        return foo(y)
+        return foo(y);
     }
-};
+}
 
 console.log("result foo(5)(2)(): ", foo(5)(2)());
 console.log("result foo(5)(): ", foo(5)());
