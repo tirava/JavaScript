e5730c00a8cac260444f0b4433dbe4921ba83619
Eugene Klimov <tirava@gmail.com>
2019-10-19 22:37:03 +0300

    HW-7 video-3

--- 7/films.js
+++ 7/films.js
@@ -15,15 +15,24 @@ class Film {
         this.category = cat;
         this.comments = [];
     }
+
     addComment(text, author, stars) {
         this.comments.push(new Comment(text, author, stars));
+    }
+
+    getAverageStars() {
+        let sumStars = 0;
+        this.comments.forEach(comment => sumStars += comment.stars);
+        return sumStars / this.comments.length;
     }
 }
 
 films.push(new Film("Титаник", 0));
 films[0].addComment("очень милый фильм", "user", 4);
+films[0].addComment("относительно милый фильм", "user", 3);
 films.push(new Film("Один дома", 0));
 films[1].addComment("очень смешной фильм", "user", 5);
+films[1].addComment("совсем не смешной фильм", "user", 1);
 
-console.log(films);
-console.log(films[0].comments[0]);
+console.log(films[0].getAverageStars());
+console.log(films[1].getAverageStars());
