030fc2e5f170d223d85898a2dd49d2e8ea2782a6
Eugene Klimov <tirava@gmail.com>
2019-10-20 14:45:05 +0300

    HW-9 video-3

--- 9/events.js
+++ 9/events.js
@@ -1,5 +1,18 @@
-function onClick() {
-    const films = getFilmsByCategory("Юмор");
+document.addEventListener("DOMContentLoaded", function () {
+    for (let category of categories) {
+        const newEl = document.createElement("div");
+        newEl.classList.add("category");
+        newEl.innerText = category;
+        newEl.addEventListener("click", function () {
+            onCategoryChoice(category)
+        });
+        document.querySelector(".categories").appendChild(newEl);
+    }
+});
+
+function onCategoryChoice(categoryName) {
+    document.querySelector(".films").innerHTML = "";
+    const films = getFilmsByCategory(categoryName);
     for (let film of films) {
         const newEl = document.createElement("div");
         newEl.classList.add("film");
